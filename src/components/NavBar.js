d20b91de8a3af9098bf179078c36d65c743ed639
Mark Brian <markbkiunga2017@gmail.com>
2024-08-02 09:27:47 +0300

    Completes Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React from 'react';
 
 function NavBar() {
-  const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const links = ['home', 'about', 'projects'];
+  const link = links.map((link) => {
+    return (
+      <a key={link} href={'#' + link}>
+        {link}
+      </a>
+    );
+  });
+  return (
+    <nav>
+      {/* display an <a> tag for each link here */}
+      {link}
+    </nav>
+  );
 }
 
 export default NavBar;
